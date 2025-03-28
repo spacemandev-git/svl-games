88c2a9f46339434fcbac19648d77061673775ad1
spacemandev <devbharel@gmail.com>
2025-03-28 02:02:10 -0500

    added postgres adapter to server logic

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { Server, Origins } from 'boardgame.io/server';
 import { airlandsea } from './lib/games/airlandsea';
+import {PostgresStore} from 'bgio-postgres';
+
+const db = new PostgresStore(process.env.DB_URL!);
 
 const server = Server({
     games: [airlandsea],
     origins: [Origins.LOCALHOST],
+    db,
 });
 
 const PORT = 8000;
